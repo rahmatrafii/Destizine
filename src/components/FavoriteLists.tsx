2915e920be07606114105e45217730bedb718778
rahmat rafii <rahmatrafiindrayani@gmail.com>
2023-10-30 02:30:55 +0700

    menghapus import react

--- src/components/FavoriteLists.tsx
+++ src/components/FavoriteLists.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useTranslation } from "react-i18next";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { A11y, Navigation, Pagination } from "swiper/modules";
